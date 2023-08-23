cd5cd6de522dc4317a3d90e0b212674afd30b88e
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-08-23 15:59:19 +0200

    minor fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME') }} | @yield('title')</title>
+    <style>
+        body {
+            display: none
+        }
+    </style>
     {{-- Style --}}
     @vite('resources/js/app.js')
 </head>
